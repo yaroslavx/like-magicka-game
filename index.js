09a0d4dd2d86d03b3a0955d36c6860e95754d639
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-03-24 12:28:52 +0300

    Implementet collision detection helper function

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ window.addEventListener("load", function () {
       }
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
+
+      // Collision player position with obstacles
+      this.game.obstacles.forEach((obstacle) => {
+        if (this.game.checkCollision(this, obstacle)) {
+          console.log("something");
+        }
+      });
     }
   }
 
@@ -132,6 +139,14 @@ window.addEventListener("load", function () {
       this.player.draw(context);
       this.player.update();
       this.obstacles.forEach((obstacle) => obstacle.draw(context));
+    }
+
+    checkCollision(a, b) {
+      const dx = a.collisionX - b.collisionX;
+      const dy = a.collisionY - b.collisionY;
+      const distance = Math.hypot(dy, dx);
+      const sumOfRadii = a.collisionRadius + b.collisionRadius;
+      return distance < sumOfRadii;
     }
 
     init() {
